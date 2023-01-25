01947522aeaf5ad424fb6b106a2bd96a0d5ca539
Sharon Okeakwalam <sharonokeakwalam@gmail.com>
2023-01-25 22:12:28 +0000

    clean data up

--- calculator.py
+++ calculator.py
@@ -1,3 +1,20 @@
+import re
 
-            
-            
+class calculate:
+    def __init__(self, calculator_input):
+        self.calculator_input = calculator_input.rstrip()
+    
+    def split_input(self):
+        splitted_output = re.split(r'([()^/*+-])', self.calculator_input)
+        return splitted_output
+          
+    def calculation(self):
+        data = self.split_input()
+        for i in data:
+            return data
+        
+      
+
+        
+                
+                
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from calculator import calculate as C
+
 class main():
 
     print("Calculate here...")
-    inputed_data = [input()]
-    print(int(inputed_data))
+    inputed_data = input()
+    calculator_class = C(inputed_data)
+    
+    main_calculation = calculator_class.calculation()
 
+    
+
+    
+
+    
+
+    
+    
     
 if __name__ == "__main__":
     main()
