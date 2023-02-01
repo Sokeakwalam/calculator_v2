f1a25ab8a3e1d25ac4c94c01b23312b81e9a590d
Sharon Okeakwalam <sharonokeakwalam@gmail.com>
2023-02-01 07:34:47 +0000

    calculator before review

--- calculator.py
+++ calculator.py
@@ -1,20 +1,54 @@
 import re
+import operators as O
 
-class calculate:
+class Calculate:
     def __init__(self, calculator_input):
-        self.calculator_input = calculator_input.rstrip()
-    
+        self.calculator_input = calculator_input
+
     def split_input(self):
-        splitted_output = re.split(r'([()^/*+-])', self.calculator_input)
-        return splitted_output
-          
-    def calculation(self):
-        data = self.split_input()
-        for i in data:
-            return data
+        split_data = re.split(r'([()^/*+-])', self.calculator_input)
+        splitted_input = [x for x in split_data if x != '']
+        return splitted_input
+
+    def brackets(self, data):
+        while "(" and ")" in data:
+            pos1 = data.index("(")
+            pos2 = data.index(")")
+            bracket_data = data[pos1+1:pos2]
+            while "^" in bracket_data:
+                O.power_of(bracket_data)
+            while "*" in bracket_data:
+                O.multiplication(bracket_data)
+            while "/" in bracket_data:
+                O.division(bracket_data)
+            while "+" in bracket_data:
+                O.addition(bracket_data)
+            while "-" in bracket_data:
+                O.subtraction(bracket_data)
+            data[pos1] = bracket_data[0]
+            del data[pos1+1:pos2+1]    
+    
+    def no_brackets(self,data):
+        while "^" in data:
+            O.power_of(data)
+        while "*" in data:
+            O.multiplication(data)
+        while "/" in data:
+            O.division(data)
+        while "+" in data:
+            O.addition(data)
+        while "-" in data:
+            O.subtraction(data)
         
-      
+    def calculations(self):
+        data = self.split_input()
+
+        self.brackets(data)
+        self.no_brackets(data)
+        return data
 
         
+        
+    
                 
-                
+        
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from calculator import calculate as C
-
+from calculator import Calculate as C
 class main():
 
-    print("Calculate here...")
-    inputed_data = input()
-    calculator_class = C(inputed_data)
-    
-    main_calculation = calculator_class.calculation()
+    def main_calculation():
+        print("Calculate here...")
+        inputed_data = input()
+
+        user_input = C(inputed_data)
+        
+        answer = user_input.calculations()
+        if len(answer) > 1:
+            for i in range(len(answer)-1):
+                final_answer = answer[i] * answer[i+1]
+            return final_answer
+        else:
+            return answer[0]
+    print(main_calculation())
 
     
-
-    
-
-    
-
-    
-    
-    
+   
 if __name__ == "__main__":
     main()
